f31d2e1061e670f773f28c5eed89dd25b50a3526
gaboesquivel <contact@gaboesquivel.com>
2017-08-22 10:54:39 -0600

    first commit

---
+++ content/contact.md
@@ -0,0 +1,5 @@
++++
+description = ""
+title = "Get in Touch"
+type = "contact"
++++
---
+++ static-src/js/index.js
@@ -0,0 +1,14 @@
+// import libs
+global.jQuery = global.$ = require('jquery')
+require('bootstrap-sass')
+require('./libs/jquery.unveil')
+require('./libs/jquery.easing.1.3')
+require('./libs/jquery.waypoints.min')
+require('./libs/jquery.stellar.min')
+require('./libs/jquery.countTo')
+require('./libs/jquery.magnific-popup.min')
+
+// import site scripts
+require('./magnific-popup-options')
+// require('./google_map')
+require('./main')
