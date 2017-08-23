189bde50ad3822e74a6c03c8252353aeba22dc47
gaboesquivel <contact@gaboesquivel.com>
2017-08-22 20:50:31 -0600

    add particlejs

--- static-src/js/index.js
+++ static-src/js/index.js
@@ -1,6 +1,7 @@
 // import libs
 global.jQuery = global.$ = require('jquery')
 require('bootstrap-sass')
+require('particles.js')
 require('./libs/jquery.unveil')
 require('./libs/jquery.easing.1.3')
 require('./libs/jquery.waypoints.min')
@@ -12,3 +13,11 @@ require('./libs/jquery.magnific-popup.min')
 require('./magnific-popup-options')
 // require('./google_map')
 require('./main')
+
+
+particlesJS.load('particles', 'assets/particlesjs-config.json', function(err) {
+  if(err){
+    return console.log(err)
+  }
+  console.log('callback - particles.js config loaded');
+});
